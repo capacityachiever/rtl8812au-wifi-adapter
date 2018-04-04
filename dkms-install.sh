43e1fbc302af54b37c2d2265d6a4b66dbe1d41d4
Harry Shannon <capacityachiever@gmail.com>
2018-04-04 21:10:17 +1000

    Reorganization to clear the clutter at the root directory

--- dkms-install.sh
+++ dkms-install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd rtl8812au
+
 if [[ $EUID -ne 0 ]]; then
-  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-install.sh\"" 2>&1
-  exit 1
+  echo "You must run this with superuser priviliges.  We will now try \"sudo ./dkms-install.sh\"" 2>&1
+  sudo bash -c "$0"
+  #exit 1
 else
   echo "About to run dkms install steps..."
 fi
@@ -16,15 +19,17 @@ DRV_DIR=rtl8812au
 DRV_NAME=rtl8812au
 DRV_VERSION=5.1.5
 
-cp -r ../${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
+sudo cp -r ../${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
 
-dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+sudo dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+sudo dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+sudo dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
 
 #Harish: add the driver to the list of services to be autoloaded at startup
 # This solves a big issue with wifi after everything is done..
-echo 8812au  >> /etc/modules  # echo 8812au | sudo tee -a /etc/modules
+if [[ "$(grep -i 8812au /etc/modules)" == "" ]]; then 
+echo 8812au  | sudo tee -a /etc/modules  # echo 8812au | sudo tee -a /etc/modules
+fi
 
 # Harish: Most important to avoid physical replugging in the usb antenna
 # All below solutions found after searching for "restart usb port/power-on/off usb port/ avoid pluggingin etc"
@@ -35,7 +40,7 @@ echo 8812au  >> /etc/modules  # echo 8812au | sudo tee -a /etc/modules
 # sudo service network-manager restart
 # sudo nmcli radio wifi off
 # WORKING:
-modprobe 8812au
+sudo modprobe 8812au
 
 RESULT=$?
 
